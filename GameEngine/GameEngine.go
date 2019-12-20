670566a834c61801b2ff966d356a4b3e7c5a6995
Kevin Colyer <kevin@ywambrussels.be>
2019-12-20 18:09:44 +0000

    converted engine to use float64 throughout

--- GameEngine/GameEngine.go
+++ GameEngine/GameEngine.go
@@ -2,84 +2,90 @@ package GameEngine
 
 import (
 	"fmt"
-	"github.com/veandco/go-sdl2/sdl"
+	"math"
 	"math/rand"
 	"os"
-    "time"
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 type Context struct {
-    WindowTitle string
-    WinWidth int32
-    WinHeight int32
-	Window *sdl.Window
-	Renderer *sdl.Renderer
-    Blocks int64
-    ScrnWidth int64
-    ScrnHeight int64
-       }
+	WindowTitle string
+	WinWidth    float64
+	WinHeight   float64
+	Window      *sdl.Window
+	Renderer    *sdl.Renderer
+	Blocks      float64
+	ScrnWidth   float64
+	ScrnHeight  float64
+	lastTick    time.Time
+}
 
+var ctx Context
 
-var ctx Context 
-
-func New(b,sw,sh int, title string) *Context {
-    fmt.Println("starting Game engine")
-    ctx = Context{
-        Blocks: int64(b),
-        ScrnWidth: int64(sw),
-        ScrnHeight: int64(sh),
-        WinWidth: int32(sw*b),
-        WinHeight: int32(sh*b),
-        WindowTitle: title,
-    };
-    var err error
-    ctx.Window, err = sdl.CreateWindow(ctx.WindowTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		ctx.WinWidth, ctx.WinHeight, sdl.WINDOW_SHOWN)
+func New(b, sw, sh float64, title string) *Context {
+	fmt.Println("starting Game engine")
+	ctx = Context{
+		Blocks:      b,
+		ScrnWidth:   sw,
+		ScrnHeight:  sh,
+		WinWidth:    sw * b,
+		WinHeight:   sh * b,
+		WindowTitle: title,
+		lastTick:    time.Now(),
+	}
+	var err error
+	ctx.Window, err = sdl.CreateWindow(ctx.WindowTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		int32(ctx.WinWidth), int32(ctx.WinHeight), sdl.WINDOW_SHOWN)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create window: %s\n", err)
-		os.Exit( 1)
+		os.Exit(1)
 	}
 
 	ctx.Renderer, err = sdl.CreateRenderer(ctx.Window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
-		os.Exit( 2)
+		os.Exit(2)
 	}
 
-    return &ctx
+	return &ctx
 }
 
+// cleans up window and renderer
 func (c *Context) Destroy() {
 	c.Window.Destroy()
 	c.Renderer.Destroy()
-    
 }
 
-func NewRect(x, y, w, h int32) *sdl.Rect {
-    return &sdl.Rect{x, y, w, h}
+// helper function to build and SDL rectangle from float64's
+func NewRect(x, y, w, h float64) *sdl.Rect {
+	return &sdl.Rect{int32(x), int32(y), int32(w), int32(h)}
 }
 
+// Delay in milliseconds
 func Delay(s uint32) {
-    sdl.Delay(s)
+	sdl.Delay(s)
 }
 
 // rand intn for sdl
-func RandIntN(i int) int32 {
-    return int32(rand.Intn(i))
+func RandIntN(i float64) float64 {
+	return float64(rand.Intn(int(i)))
 }
 
-
-func Wrap(num, low, hi int32) int32 {
+// returns number wraped around a low and hi boundary
+func Wrap(num, low, hi float64) float64 {
 	if num < low {
-		return hi
+		return hi - (low - num)
 	}
 	if num > hi {
-		return low
+		return low + (num - hi)
 	}
 	return num
 }
 
-func Clamp(num, low, hi int32) int32 {
+// returns number clamped between low and hi
+func Clamp(num, low, hi float64) float64 {
 	if num < low {
 		return low
 	}
@@ -89,6 +95,7 @@ func Clamp(num, low, hi int32) int32 {
 	return num
 }
 
+// returns number clamped between 0 and 1
 func Clamp01(num float64) float64 {
 	if num < 0 {
 		return 0
@@ -99,13 +106,19 @@ func Clamp01(num float64) float64 {
 	return num
 }
 
+// random number 0-255
 func R256() uint8 {
 	return uint8(rand.Intn(256))
 }
 
-func (c *Context) Line(x0, y0, x1, y1 int32) {
-    blocks:=int32(c.Blocks)
-    
+// line drawing (blocks)
+func (c *Context) Line(x0, y0, x1, y1 float64) {
+	blocks := c.Blocks
+	x0 = math.Round(x0)
+	x1 = math.Round(x1)
+	y0 = math.Round(y0)
+	y1 = math.Round(y1)
+
 	// from rosetta code
 	dx := x1 - x0
 	if dx < 0 {
@@ -115,7 +128,7 @@ func (c *Context) Line(x0, y0, x1, y1 int32) {
 	if dy < 0 {
 		dy = -dy
 	}
-	var sx, sy int32
+	var sx, sy float64
 	if x0 < x1 {
 		sx = 1
 	} else {
@@ -129,7 +142,7 @@ func (c *Context) Line(x0, y0, x1, y1 int32) {
 	err := dx - dy
 
 	for {
-		c.Renderer.FillRect(&sdl.Rect{x0 * blocks, y0 * blocks, blocks, blocks})
+		c.Renderer.FillRect(NewRect(x0*blocks, y0*blocks, blocks, blocks))
 		if x0 == x1 && y0 == y1 {
 			break
 		}
@@ -139,16 +152,16 @@ func (c *Context) Line(x0, y0, x1, y1 int32) {
 			x0 += sx
 		}
 		if e2 < dx {
-            err += dx
+			err += dx
 			y0 += sy
 		}
 	}
 }
 
-func (c *Context) Triangle(x0, y0, x1, y1, x2, y2 int32) {
-// 	blocks:=int32(c.Blocks)
-    
-    if x0 == x1 && x1 == x2 && y0 == y1 && y1 == y2 {
+// Draws outline Triangle (blocks)
+func (c *Context) Triangle(x0, y0, x1, y1, x2, y2 float64) {
+
+	if x0 == x1 && x1 == x2 && y0 == y1 && y1 == y2 {
 		c.Point(x0, y0)
 		return
 	}
@@ -174,7 +187,7 @@ func (c *Context) Triangle(x0, y0, x1, y1, x2, y2 int32) {
 
 	} else {
 		//get new vertex in middle of x0,y0 x2,y2 face at y1
-		x3 := int32(float64(x0) + (float64(y1-y0) / float64(y2-y0) * float64(x2-x0)))
+		x3 := x0 + (y1-y0)/(y2-y0)*(x2-x0)
 		y3 := y1
 		//bf
 		c.bottomFlatTriangle(x0, y0, x1, y1, x3, y3)
@@ -184,45 +197,50 @@ func (c *Context) Triangle(x0, y0, x1, y1, x2, y2 int32) {
 	}
 }
 
-func (c *Context) bottomFlatTriangle(x1, y1, x2, y2, x3, y3 int32) {
-// 	blocks:=int32(c.Blocks)
-	invslope1 := float64(x2-x1) / float64(y2-y1)
-	invslope2 := float64(x3-x1) / float64(y3-y1)
-	curx1 := float64(x1)
-	curx2 := float64(x1)
+// helper for Trinagle
+func (c *Context) bottomFlatTriangle(x1, y1, x2, y2, x3, y3 float64) {
+	// 	blocks:=int32(c.Blocks)
+
+	invslope1 := (x2 - x1) / (y2 - y1)
+	invslope2 := (x3 - x1) / (y3 - y1)
+	curx1 := x1
+	curx2 := x1
 
 	for scanlineY := y1; scanlineY <= y2; scanlineY++ {
-		c.Line(int32(curx1), scanlineY, int32(curx2), scanlineY)
+		c.Line(curx1, scanlineY, curx2, scanlineY)
 		curx1 += invslope1
 		curx2 += invslope2
+
 	}
 }
 
-func (c *Context) topFlatTriangle(x1, y1, x2, y2, x3, y3 int32) {
-// 	blocks:=int32(c.Blocks)
-	invslope1 := float64(x3-x1) / float64(y3-y1)
-	invslope2 := float64(x3-x2) / float64(y3-y2)
-	curx1 := float64(x3)
-	curx2 := float64(x3)
+// helper for Triangle
+func (c *Context) topFlatTriangle(x1, y1, x2, y2, x3, y3 float64) {
+
+	invslope1 := (x3 - x1) / (y3 - y1)
+	invslope2 := (x3 - x2) / (y3 - y2)
+	curx1 := x3
+	curx2 := x3
 
 	for scanlineY := y3; scanlineY > y1; scanlineY-- {
-		c.Line(int32(curx1), scanlineY, int32(curx2), scanlineY)
+		c.Line(curx1, scanlineY, curx2, scanlineY)
 		curx1 -= invslope1
 		curx2 -= invslope2
 	}
 }
 
-func (c *Context) Point(x0, y0 int32) {
-    blocks:=int32(c.Blocks)
-	c.Renderer.FillRect(&sdl.Rect{x0 * blocks, y0 * blocks, blocks, blocks})
+// Draws a point (blocks)
+func (c *Context) Point(x0, y0 float64) {
+	c.Renderer.FillRect(NewRect(x0*c.Blocks, y0*c.Blocks, c.Blocks, c.Blocks))
 }
 
-var lastTick=time.Now()
-
-func (c *Context) Elapsed() float32 {
-    t:=time.Now()
-    elapsed := t.Sub(lastTick)
-    lastTick=t
-    if elapsed==0 { elapsed++ }
-    return float32(1/float32(elapsed)*1000*1000)
+// calculates the elapsed time between updates
+func (c *Context) Elapsed() float64 {
+	t := time.Now()
+	elapsed := float64(t.Sub(c.lastTick))
+	c.lastTick = t
+	if elapsed == 0 {
+		elapsed++
+	}
+	return 1 / elapsed * 1000 * 1000
 }
--- main.go
+++ main.go
@@ -1,84 +1,83 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	. "github.com/kevincolyer/GameEngine/GameEngine"
-    "github.com/veandco/go-sdl2/sdl"
-    "os"
-    "fmt"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
-    var ctx=New(8,100,80,"Asteroids")
-    defer ctx.Destroy()
-    
-    onCreate(ctx);
-    var running = true
-    
-    for running {
-        running= onUpdate(ctx,ctx.Elapsed())
-    }
-    
+	var ctx = New(8, 160, 100, "Asteroids")
+	defer ctx.Destroy()
+
+	onCreate(ctx)
+	var running = true
+
+	for running {
+		running = onUpdate(ctx, ctx.Elapsed())
+	}
+
 	os.Exit(0)
 }
 
-var dx, dy float32
-var x, y int32
-var w, h int32
-var blocks,blocksw,blocksh int
+var dx, dy float64
+var x, y float64
+var w, h float64
+var blocks, blocksw, blocksh float64
 
 func onCreate(c *Context) {
-    fmt.Println("created")
+	fmt.Println("created")
 	w = 100
 	h = 100
 
 	dx = 2
 	dy = 2
 
-	blocks = int(c.Blocks)
-	blocksw = int(c.ScrnWidth)
-	blocksh = int(c.ScrnHeight)  
-    
+	blocks = c.Blocks
+	blocksw = c.ScrnWidth
+	blocksh = c.ScrnHeight
+	c.Renderer.Clear()
+
 }
 
-func onUpdate(c *Context, elapsed float32) (running bool) {
-	var oldLx, oldLy, nx, ny int32
+func onUpdate(c *Context, elapsed float64) (running bool) {
+	var oldLx, oldLy, nx, ny float64
 	running = true
-fmt.Println(elapsed)
 
-		x += int32(dx*elapsed)
-		y += int32(dy*elapsed)
-		x = Wrap(x, 0, c.WinWidth)
-		y = Wrap(y, 0, c.WinHeight)
-        R:=NewRect(x,y,w,h)
-        
-		c.Renderer.SetDrawColor(255, 127, 127, 255)
-		c.Renderer.FillRect( R )
-		c.Renderer.SetDrawColor(0, 0, 0, 255)
-		c.Renderer.DrawRect( R )
+	x += dx * elapsed
+	y += dy * elapsed
+	x = Wrap(x, 0, c.WinWidth)
+	y = Wrap(y, 0, c.WinHeight)
+	
+	R := NewRect(x, y, w, h)
+	c.Renderer.SetDrawColor(255, 127, 127, 255)
+	c.Renderer.FillRect(R)
+	c.Renderer.SetDrawColor(0, 0, 0, 255)
+	c.Renderer.DrawRect(R)
 
-		c.Renderer.SetDrawColor(R256(), R256(), R256(), 255)
-		nx = RandIntN(blocksw)
-		ny = RandIntN(blocksh)
-		c.Line(oldLx, oldLy, nx, ny)
-		oldLx = nx
-		oldLy = ny
+	c.Renderer.SetDrawColor(R256(), R256(), R256(), 255)
+	nx = RandIntN(blocksw)
+	ny = RandIntN(blocksh)
 
-		c.Renderer.SetDrawColor(R256(), R256(), R256(), 255)
-		c.Triangle(RandIntN(blocksw), RandIntN(blocksh), RandIntN(blocksw), RandIntN(blocksh), RandIntN(blocksw), RandIntN(blocksh))
+	c.Line(oldLx, oldLy, nx, ny)
+	oldLx = nx
+	oldLy = ny
 
-		c.Renderer.Present()
+	c.Renderer.SetDrawColor(R256(), R256(), R256(), 255)
+	c.Triangle(RandIntN(blocksw), RandIntN(blocksh), RandIntN(blocksw), RandIntN(blocksh), RandIntN(blocksw), RandIntN(blocksh))
 
-		Delay(1)
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				println("Quit")
-				running = false
-				break
-			}
+	c.Renderer.Present()
+	Delay(1)
+	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch event.(type) {
+		case *sdl.QuitEvent:
+			println("Quit")
+			running = false
+			break
 		}
+	}
 
-    return running
+	return running
 }
-
-
